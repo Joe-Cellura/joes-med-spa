bc6733e067b39773d3d9d6d1b33089bafe0b45f3
Joe Cellura <joe.cellura1@gmail.com>
2026-03-25 15:20:45 -0400

    set glo-de-vie branch to glo-de-vie

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 // Change this value to switch between clients
-export const ACTIVE_CLIENT = "lumina";
+export const ACTIVE_CLIENT = "glo-de-vie";
 
 export const CLIENT_DATA_PATH = `../data/clients/${ACTIVE_CLIENT}`;
